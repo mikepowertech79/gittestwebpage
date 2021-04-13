d3f66f79ff73046bc06405bb14f3cfb943f080bc
mikepowertech79 <mikepowertech79@gmail.com>
2021-04-13 12:17:50 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gittestwebpage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
